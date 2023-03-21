2955cca9b77a3d7149c6eb381dd95f015ada8e4b
mdtanvirgit <officialtanvir@hotmail.com>
2023-03-21 09:34:32 +0600

    I am search for binary

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const frinds = ["তানভীর","শামিম","জসিম","তাওহিদ","আনিকাি","জসিম"];
+
+const binarySearch = (exampleArray,target) =>{
+    let start = 0;
+    let end = exampleArray.length - 1;
+
+    while (start <= end) {
+        let med = Math.floor((start+end) /2);
+        if(exampleArray[med] === target){
+            console.log(`item found in position ${med}`)
+            return med
+        }
+        if(exampleArray[med]<target){
+            start = med + 1
+        }
+        if(exampleArray[med]>target){
+            end = med -1
+        }
+       
+    }
+    return -1;
+}
+
+const index = binarySearch([1,2,3,5,7,8,10,13,16,18,20,23,25,28,29,30,37,38,40,45],10);
+
+console.log(index);
